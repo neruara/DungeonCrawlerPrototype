5faa692d2d1ce02b91497df4fbe03527fe44ed10
Ara Neru <tolgakiremitci1@gmail.com>
2026-07-24 15:17:45 +0300

    Added an Orb Weapon
    Added a basic Orb Weapon

--- Assets/Scripts/EquipmentInteraction.cs
+++ Assets/Scripts/EquipmentInteraction.cs
@@ -6,12 +6,16 @@ public class EquipmentInteraction: MonoBehaviour
     public float interactionDistance = 3f;
     [Header ("References")]
     public GameObject playerSword;
+    public GameObject playerOrb;
     private Camera mainCam;
 
     void Start(){
         mainCam = Camera.main;
         if(playerSword != null) {
             playerSword.SetActive(false);
+        }
+        if(playerOrb != null) {
+            playerOrb.SetActive(false);
         }
     }
     void Update(){
@@ -24,8 +28,16 @@ public class EquipmentInteraction: MonoBehaviour
         RaycastHit hitInfo;
         if(Physics.Raycast (ray, out hitInfo, interactionDistance)){
             if(hitInfo.collider.CompareTag("SwordPickup")){
+                if(playerOrb.activeInHierarchy == false){
+                    Destroy(hitInfo.collider.gameObject);
+                    playerSword.SetActive(true);
+                }
+            }
+            if(hitInfo.collider.CompareTag("OrbPickup")){
+                if(playerSword.activeInHierarchy == false){
                 Destroy(hitInfo.collider.gameObject);
-                playerSword.SetActive(true);
+                playerOrb.SetActive(true);
+                }
             }
         }
     }
